c02160f3503b1e04af42c9d24129e8021261d24e
Admin <khoa78479@st.vimaru.edu.vn>
2022-03-17 22:10:31 +0700

    Update file face_ricognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -18,10 +18,10 @@ modeldir = './model/20180402-114759.pb'
 classifier_filename = './class/classifier.pkl'
 npy = './npy'
 train_img = "./train_img"
-Frames = []
-countFrame = 20
-
 serverName = "autodoor.herokuapp.com"
+link_list = []
+node_size = 10
+temp = 0
 
 
 def responseSever(serverName, message):
@@ -41,41 +41,30 @@ def responseSever(serverName, message):
     ws.close()
 
 
-def FrameCount(Frames, serverName, message):
-    average = 0.00
-    total = 0.00
-    for Frame in Frames:
-        total += Frame["accuracy"]
-    average = total/countFrame
-    if((average) >= 0.8 and len(Frames) == countFrame):
-        print("Average accuracy: {:.3f}%".format(average*100))
-        responseSeverThr = threading.Thread(
-            target=responseSever, args=[serverName, message])
-        responseSeverThr.start()
+def AccuracyStatistics(Nodes):
+    count = 0
+    for Node in Nodes:
+        if Node[1] >= 0.85:
+            count += 1
+    return count
 
 
-def fancyDraw(img, bbox, l=30, t=3, rt=1):
+def fancy_draw(img, bbox, l=30, t=3, rt=1):
     xmin, ymin, xmax, ymax = bbox
-
     # Bounding box
     cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (0, 255, 0), rt)
-
     # Top Left  x,y
     cv2.line(img, (xmin, ymin), (xmin + l, ymin), (0, 255, 0), t)
     cv2.line(img, (xmin, ymin), (xmin, ymin + l), (0, 255, 0), t)
-
     # Top Right  x_max,y
     cv2.line(img, (xmax, ymin), (xmax - l, ymin), (0, 255, 0), t)
     cv2.line(img, (xmax, ymin), (xmax, ymin + l), (0, 255, 0), t)
-
     # Bottom Left  x,y_max
     cv2.line(img, (xmin, ymax), (xmin + l, ymax), (0, 255, 0), t)
     cv2.line(img, (xmin, ymax), (xmin, ymax - l), (0, 255, 0), t)
-
     # Bottom Right  x_max,y_max
     cv2.line(img, (xmax, ymax), (xmax - l, ymax), (0, 255, 0), t)
     cv2.line(img, (xmax, ymax), (xmax, ymax - l), (0, 255, 0), t)
-
     return img
 
 
@@ -86,15 +75,14 @@ with tf.Graph().as_default():
     with sess.as_default():
         pnet, rnet, onet = detect_face.create_mtcnn(sess, npy)
         minsize = 30
-        threshold = [0.7, 0.7, 0.7]
+        threshold = [0.7, 0.8, 0.8]
         factor = 0.709
         margin = 44
         batch_size = 1000
-        image_size = 500
+        image_size = 182
         input_image_size = 160
         HumanNames = os.listdir(train_img)
         HumanNames.sort()
-        print(HumanNames)
         print('Loading Model')
         facenet.load_model(modeldir)
         images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
@@ -104,12 +92,11 @@ with tf.Graph().as_default():
         classifier_filename_exp = os.path.expanduser(classifier_filename)
         with open(classifier_filename_exp, 'rb') as infile:
             (model, class_names) = pickle.load(infile, encoding='latin1')
-
         video_capture = cv2.VideoCapture(video)
         print('Start Recognition')
-
         while True:
             ret, frame = video_capture.read()
+            frame = cv2.flip(frame, 1)
             timer = time.time()
             if frame.ndim == 2:
                 frame = facenet.to_rgb(frame)
@@ -117,17 +104,11 @@ with tf.Graph().as_default():
                 frame, minsize, pnet, rnet, onet, threshold, factor)
             faceNum = bounding_boxes.shape[0]
             if faceNum > 0:
-                if faceNum > 1:
-                    cv2.putText(frame, "Warning: The number of faces is more than 1", (10, 100),
-                                cv2.FONT_HERSHEY_SIMPLEX,
-                                0.7, (0, 0, 255), 1)
-
                 det = bounding_boxes[:, 0:4]
                 img_size = np.asarray(frame.shape)[0:2]
                 cropped = []
                 scaled = []
                 scaled_reshape = []
-
                 for i in range(faceNum):
                     emb_array = np.zeros((1, embedding_size))
                     xmin = int(det[i][0])
@@ -135,16 +116,12 @@ with tf.Graph().as_default():
                     xmax = int(det[i][2])
                     ymax = int(det[i][3])
                     bbox = xmin, ymin, xmax, ymax
-
                     try:
-                        # inner exception
                         if xmin <= 0 or ymin <= 0 or xmax >= len(frame[0]) or ymax >= len(frame):
                             print('Face is very close!')
                             continue
-
                         cropped.append(frame[ymin:ymax, xmin:xmax, :])
                         cropped[i] = facenet.flip(cropped[i], False)
-
                         scaled.append(np.array(Image.fromarray(
                             cropped[i]).resize((image_size, image_size))))
                         scaled[i] = cv2.resize(scaled[i], (input_image_size, input_image_size),
@@ -152,63 +129,49 @@ with tf.Graph().as_default():
                         scaled[i] = facenet.prewhiten(scaled[i])
                         scaled_reshape.append(
                             scaled[i].reshape(-1, input_image_size, input_image_size, 3))
-
                         feed_dict = {
                             images_placeholder: scaled_reshape[i], phase_train_placeholder: False}
-
                         emb_array[0, :] = sess.run(
                             embeddings, feed_dict=feed_dict)
-
                         predictions = model.predict_proba(emb_array)
-
                         best_class_indices = np.argmax(predictions, axis=1)
                         best_class_probabilities = predictions[np.arange(
                             len(best_class_indices)), best_class_indices]
-
-                        if best_class_probabilities > 0.93:
-                            fancyDraw(frame, bbox)
-                            for H_i in HumanNames:
-                                if HumanNames[best_class_indices[0]] == H_i:
-                                    result_names = HumanNames[best_class_indices[0]]
-
-                                    print(i, " --Predictions : [ name: {} , accuracy: {:.3f} ]".format(
-                                        HumanNames[best_class_indices[0]], best_class_probabilities[0]))
-
-                                    Frames.append({
-                                        "name": HumanNames[best_class_indices[0]],
-                                        "accuracy": best_class_probabilities[0]
-                                    })
-                                    if len(Frames) > countFrame:
-                                        Frames = []
-
-                                    cv2.rectangle(
-                                        frame, (xmin, ymin - 30), (xmax, ymin - 10), (0, 255, 0), -1)
-                                    cv2.putText(frame, result_names, (xmin, ymin - 12), cv2.FONT_HERSHEY_COMPLEX_SMALL,
-                                                1, (0, 0, 0), thickness=1, lineType=1)
-                            message = "FACE_RECOGNITION_CONFIRM"
-                            FrameCountThr = threading.Thread(target=FrameCount, args=[
-                                                             Frames, serverName, message])
-                            FrameCountThr.start()
+                        fancy_draw(frame, bbox)
+                        if best_class_probabilities > 0.80:
+                            if faceNum != temp:
+                                temp = faceNum
+                                link_list = []
+                                for i in range(0, faceNum):
+                                    link_list.append([])
+                            print(len(link_list), '\n')
+                            print('i: ', i)
+                            link_list[i].append(
+                                [HumanNames[best_class_indices[0]], best_class_probabilities])
+                            link_list[i] = link_list[i][-node_size:]
+                            print(link_list)
+                            result_names = ""
+                            if AccuracyStatistics(link_list[i]) >= (node_size*(80/100)):
+                                result_names = HumanNames[best_class_indices[0]]
+                            else:
+                                result_names = "Unknown"
+                            cv2.rectangle(frame, (xmin, ymin - 30),
+                                          (xmax, ymin - 10), (0, 255, 0), -1)
+                            cv2.putText(frame, result_names, (xmin, ymin - 12), cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                                        1, (0, 0, 0), thickness=1, lineType=1)
                         else:
-                            fancyDraw(frame, bbox)
-                            Frames.append({
-                                "name": "Unknown",
-                                "accuracy": 0.0
-                            })
-                            if len(Frames) > countFrame:
-                                Frames = []
                             cv2.rectangle(frame, (xmin, ymin - 30),
                                           (xmax, ymin - 10), (0, 255, 0), -1)
                             cv2.putText(frame, "Unknown", (xmin, ymin - 12), cv2.FONT_HERSHEY_COMPLEX_SMALL,
                                         1, (0, 0, 0), thickness=1, lineType=1)
                     except Exception as e:
-
                         print("Error: ", e)
-
+            else:
+                temp = 0
             endtimer = time.time()
             fps = 1 / (endtimer - timer)
             cv2.putText(frame, "Fps: {:.2f}".format(
-                fps), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 1)
+                fps), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 1)
             cv2.imshow('Face Recognition', frame)
             key = cv2.waitKey(1)
             if key == 113:
